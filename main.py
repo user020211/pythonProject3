53dba363939fb9359a4aa30655d47271b734e06e
user020211 <user020211@gmail.com>
2021-03-19 21:33:36 +0200

    izmenil_poryadok

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 # This is a sample Python script.
-from random import*
-from math import*
-s=input('По Вашей заявке будут сгенерированы пароли, укажите количество и длину в формате x.y: ').split('.')
-kol=int(s[0])
-dlin=int(s[1])
-cifr=int(input('Нужны ли цифры Y или N: '))
-s1=sample([chr(i) for i in range(48, 58)], ceil(cifr*dlin/100))
-dlin-=ceil(cifr*dlin/100)
-sim=int(input('Нужны ли символы Y или N: '))
-s2=sample([chr(i) for i in range(33, 39)], ceil(sim*dlin/100))
-dlin-=ceil(sim*dlin/100)
-prop=int(input('Нужны ли Прописные буквы Y или N: '))
-s3=sample([chr(i) for i in range(65, 94)], ceil(prop*dlin/100))
-dlin-=ceil(prop*dlin/100)
-#if input('Нужны ли строчные буквы Y или N: ') =='Y':
-s4=sample([chr(i) for i in range(97, 123)], dlin)
-s5=(s1+s2+s3+s4)
-for i in range(kol):
-    shuffle(s5)
-    print(*s5, sep='')
+import random
+import math
+
+
+def dano(cifr, sim, prop, kol, dlin):
+    for _ in range(kol):
+        s1 = random.sample([chr(i) for i in range(48, 58)], math.ceil(cifr * dlin / 100))
+        a = dlin - math.ceil(cifr * dlin / 100)
+        s2 = random.sample([chr(i) for i in range(33, 39)], math.ceil(sim * dlin / 100))
+        b = a - math.ceil(sim * dlin / 100)
+        s3 = random.sample([chr(i) for i in range(65, 94)], math.ceil(prop * dlin / 100))
+        c = b - math.ceil(prop * dlin / 100)
+        s4 = random.sample([chr(i) for i in range(97, 123)], c)
+        s5 = (s1 + s2 + s3 + s4)
+        random.shuffle(s5)
+        print(*s5, sep='')
+        s1.clear(), s2.clear(), s3.clear(), s4.clear(), s5.clear()
+
+
+s = input('По Вашей заявке будут сгенерированы пароли, укажите количество и длину в формате x.y: ').split('.')
+kol = int(s[0])
+dlin = int(s[1])
+cifr = int(input('Нужны ли цифры Y или N: '))
+sim = int(input('Нужны ли символы Y или N: '))
+prop = int(input('Нужны ли Прописные буквы Y или N: '))
+dano(cifr, sim, prop, kol, dlin)
+
+
